56f21362c7a713c1c1650313330d2fbdb4b069e9
Rodrigo <rrojass@pucp.edu.pe>
2025-08-21 20:25:33 -0500

    feat: imprimiendo listas

--- src/main/java/com/pucp/lab020252/Lab020252Application.java
+++ src/main/java/com/pucp/lab020252/Lab020252Application.java
@@ -3,11 +3,18 @@ package com.pucp.lab020252;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
 public class Lab020252Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Lab020252Application.class, args);
         System.out.printf("Hola Mundo!");
+        List<String> lista = List.of("Elemento1", "Elemento2", "Elemento3");
+        for (String elemento : lista) {
+            System.out.println("elemento:" + elemento);
+        }
+        System.out.println("Fin del programa");
 	}
 }
