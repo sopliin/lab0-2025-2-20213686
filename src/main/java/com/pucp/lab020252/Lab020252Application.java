6bbe1da5cb52511561fc51f7197479a3a54829ce
Rodrigo <rrojass@pucp.edu.pe>
2025-08-21 20:20:08 -0500

    feat: hola mundoo

---
+++ src/main/java/com/pucp/lab020252/Lab020252Application.java
@@ -0,0 +1,13 @@
+package com.pucp.lab020252;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab020252Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab020252Application.class, args);
+        System.out.printf("Hola Mundo!");
+	}
+}
